1536dc10ed5059a70255e56c2818d8f23c350012
blahed <tdunn13@gmail.com>
2012-10-25 01:54:55 -0400

    new design, new post

--- helpers.rb
+++ helpers.rb
@@ -1,30 +1,27 @@
+require 'date'
 require 'rdiscount'
 
 module FrankHelpers
   def get_post_sumaries
     summaries = []
 
-    Dir[File.join(Dir.pwd, 'dynamic/words/**/index.md')].each do |d|
+    Dir[File.join(Dir.pwd, 'dynamic/words/*')].each do |d|
       post = File.read(d)
       begin
       summaries << {
         :id => post.match(/^id:\s(\d+)$/)[1],
-        :permalink => d.match(/(\/words\/[\w-]+\/)/)[1],
+        :permalink => d.match(/(\/words\/[\w-]+)/)[1],
         :title => post.match(/^title:\s([^\n]+)$/)[1],
+        :date => post.match(/^date:\s([^\n]+)$/)[1].gsub('/', ''),
         :summary => RDiscount.new(post.match(/^##\s+[^\n]+\n\n([^\n]+)$/)[1]).to_html
       }
-      rescue
-        puts post
+      rescue => e
+        puts e.message
+        puts e.backtrace.join("\n")
       end
     end
 
     summaries.sort {|x, y| y[:id] <=> x[:id] }
   end
 
-  def additional_script(path)
-    if path =~ /^\/words/
-      slug = path.match(/^\/words\/([\w-]+)\//)[1]
-      "<script src='/js/#{slug}.js' type='text/javascript'></script>" if File.exist?("static/js/#{slug}.js")
-    end
-  end
 end
--- static/js/do-what-you-do.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('#see-above').click(function(e) {
-    e.preventDefault();
-    $('#busy').css({color: '#732526', background: '#FF7D7D'});
-  });
-});
